a489f3e180d4deb4b7a369a87629f33ce2b12e35
Tomasz Faldrowicz <Tomasz.faldrowicz@inkubit.com>
2019-06-07 21:31:31 +0200

    dziala, dodane butony, odjete rzeczy malo potrzebne - stabilne

--- ProjectOne/ClientApp/app/components/userform/userform.component.ts
+++ ProjectOne/ClientApp/app/components/userform/userform.component.ts
@@ -60,7 +60,7 @@ export class UserFormComponent implements OnInit {
             firstName: ['', [Validators.required, Validators.minLength(3)]],
             lastName: ['', [Validators.required, Validators.maxLength(50)]],
             email: ['', [Validators.required, Validators.email]],
-            username: ['', [Validators.required, Validators.minLength(3)]],
+ //username: ['', [Validators.required, Validators.minLength(3)]],
             Token: ['', [TokenRequired]],
             phoneGroup: this.formBuilder.group({
                 verification: [''],
@@ -76,14 +76,14 @@ export class UserFormComponent implements OnInit {
 
         });
 
-        let userName = this.userForm.get('username');
-        if (userName) {
-            userName.valueChanges.debounceTime(1000).subscribe(value => {
-                if (value === 'admin')
-                    this.userExists = true;
-                else this.userExists = false;
-            });
-        }
+        //let userName = this.userForm.get('username');
+        //if (userName) {
+        //    userName.valueChanges.debounceTime(1000).subscribe(value => {
+        //        if (value === 'admin')
+        //            this.userExists = true;
+        //        else this.userExists = false;
+        //    });
+        //}
         let passwordGroup = this.userForm.get("pswdGroup");
         let password = this.userForm.get("pswdGroup.password");
 
@@ -106,12 +106,7 @@ export class UserFormComponent implements OnInit {
         }
     }
 
-    populateTestData() {
-        this.userForm.patchValue({
-            email: 'Walenski@wp.pl',
-            username: 'Walenski'
-        });
-    }
+    
 
 
 }
